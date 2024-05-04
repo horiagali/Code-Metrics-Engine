32c0834bdb499ed0c8858331be76601e69fab875
Horia Galitianu <H.Galitianu@student.tudelft.nl>
2024-05-04 19:58:09 +0200

    code is added

--- client/src/main/java/client/scenes/MainPageCtrl.java
+++ client/src/main/java/client/scenes/MainPageCtrl.java
@@ -1,34 +1,35 @@
-/*
- * Copyright 2021 Delft University of Technology
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package client.scenes;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.google.inject.Inject;
 
+import commons.FileCode;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 
 public class MainPageCtrl implements Initializable {
 
     private final MainCtrl mainCtrl;
 
+    private FileCode fileCode;
 
+    @FXML
+    private TextArea fileContentTextArea; // Reference to TextArea in FXML
+
+    /**
+     *
+     * @param mainCtrl
+     */
     @Inject
-    public MainPageCtrl( MainCtrl mainCtrl) {
+    public MainPageCtrl(MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
     }
 
@@ -37,5 +38,23 @@ public class MainPageCtrl implements Initializable {
 
     }
 
+    /**
+     * picks the file and creates a fileCode object th it's content
+     * @param actionEvent
+     * @throws IOException
+     */
 
+    @FXML
+    public void pickFile(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Java File");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Java Files", "*.java"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            fileCode = new FileCode(selectedFile.toPath());
+            // Set the file content to the TextArea
+            fileContentTextArea.setText(fileCode.getContent());
+        }
+    }
 }
